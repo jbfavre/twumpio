92fe4e500f10d7095998152c4736c7cf29d341ef
Jean Baptiste Favre <github@jbfavre.org>
2013-01-08 23:47:48 +0100

    Introduced binary with empty Twitter credentials configuration. Configuration part is really thin and need to be enhanced

---
+++ bin/twumpio.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+require 'tweetstream'
+require 'json'
+require 'multi_json'
+require 'redis'
+require 'hiredis'
+require_relative "../lib/activitystream"
+require_relative "../lib/twumpio"
+
+
+params = { twitter: { consumer_key:       '',
+                      consumer_secret:    '',
+                      oauth_token:        '',
+                      oauth_token_secret: '' },
+           pubsub:  { host: 'localhost', port: 6379, timeout: 0, tcp_keepalive: true } }
+Twumpio::Frontend.new(params)
